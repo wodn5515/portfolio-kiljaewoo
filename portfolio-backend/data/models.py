7b0a61f152db12d57f8593b7ea600fd5c2715ad2
wodn5515 <wodn5515@naver.com>
2020-11-28 14:10:14 +0900

    Update : Update models.py in data app Add __str__ method to class Project

--- portfolio-backend/data/models.py
+++ portfolio-backend/data/models.py
@@ -5,3 +5,6 @@ from django.utils.translation import ugettext_lazy as _
 class Project(models.Model):
     title = models.CharField(_('프로젝트명'), max_length=50)
     desc = models.TextField(_('설명'))
+
+    def __str__(self):
+        return f'{self.title}'
