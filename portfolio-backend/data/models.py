388ded97b9ea8f92d4048c080f2fa377c4360603
wodn5515 <wodn5515@naver.com>
2020-11-28 14:05:48 +0900

    Update : update models.py
    Add model Project with title, desc fields.
    <issue-number>

--- portfolio-backend/data/models.py
+++ portfolio-backend/data/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(_('프로젝트명'), max_length=50)
+    desc = models.TextField(_('설명'))
