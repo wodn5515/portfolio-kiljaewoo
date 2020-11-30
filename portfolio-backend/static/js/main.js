592bf13f0ce42b649bba2c82ce0f66a8347b3958
wodn5515 <wodn5515@naver.com>
2020-11-30 12:39:35 +0900

    Update : css
    add nav javscript function
    update css about nav

--- portfolio-backend/static/js/main.js
+++ portfolio-backend/static/js/main.js
@@ -1,4 +1,4 @@
-function navPopup(){
+function navDropdown(){
     if($(window).scrollTop() > 0){
         $('#nav').addClass('scroll');
     }else{
@@ -6,12 +6,34 @@ function navPopup(){
     }
 }
 
+function navHighlight(anchor){
+    $('#nav li.on').removeClass('on')
+    $("#nav [dest=" + anchor + "]").addClass('on')
+}
+
+function navOn(){
+    let scroll = $(window).scrollTop();
+    let scroll2 = scroll+90;
+    if(scroll2 > $('#contact')[0].offsetTop){
+        navHighlight('contact')
+    }else if(scroll2 > $('#project')[0].offsetTop){
+        navHighlight('project')
+    }else if(scroll2 > $('#skill')[0].offsetTop){
+        navHighlight('skill')
+    }else if(scroll2 > $('#about')[0].offsetTop){
+        navHighlight('about')
+    }else{
+        navHighlight('home')
+    }
+}
+
 $(window).scroll(function() {
-    navPopup();
+    navDropdown();
+    navOn();
 });
 
 $('.dest').click(function(event){
     let destId = $(event.currentTarget).attr('dest');
     let destScroll = $('#'+destId)[0].offsetTop;
-    $('html, body').animate({scrollTop : destScroll}, 400);
+    $('html, body').animate({scrollTop : destScroll-84}, 400);
 })
