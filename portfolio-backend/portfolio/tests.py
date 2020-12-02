c4944d71cfb3f48bf9d403eade33f7db2e5993dc
wodn5515 <wodn5515@naver.com>
2020-12-02 23:11:42 +0900

    Update : ' to "
    prettier changed

--- portfolio-backend/portfolio/tests.py
+++ portfolio-backend/portfolio/tests.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
 from django.template.loader import render_to_string
 from django.http import HttpRequest
-from portfolio.views import home
+from .views import home
+
 
 class HomePageTest(TestCase):
-
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home(request)
-        expected_html = render_to_string('home.html', request=request)
+        expected_html = render_to_string("home.html", request=request)
         self.assertEqual(response.content.decode(), expected_html)
