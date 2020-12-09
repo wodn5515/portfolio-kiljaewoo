f1589ca571c1e57e79696ec6d2ad296175b4b978
wodn5515 <wodn5515@naver.com>
2020-12-09 23:07:00 +0900

    Modify : data Skill structure

--- portfolio-backend/data/choices.py
+++ portfolio-backend/data/choices.py
@@ -1,1 +1,2 @@
 LEVEL_CHOICES = [("B", "Basic"), ("I", "Intermediate"), ("E", "Expert")]
+TYPE_CHOICES = [("Frontend", "Frontend"), ("Backend", "Backend"), ("DevOps", "DevOps")]
--- portfolio-backend/data/serializers.py
+++ portfolio-backend/data/serializers.py
@@ -1,14 +1,23 @@
 from rest_framework import serializers
-from .models import Skill
+from .models import Skill, SkillType
 from .choices import LEVEL_CHOICES
 
 
 class SkillSerializer(serializers.ModelSerializer):
     level = serializers.SerializerMethodField()
+    icon = serializers.ImageField(use_url=True)
 
     def get_level(self, obj):
         return obj.get_level_display()
 
     class Meta:
         model = Skill
+        fields = "__all__"
+
+
+class SkillTypeSerializer(serializers.ModelSerializer):
+    skill = SkillSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = SkillType
         fields = "__all__"
