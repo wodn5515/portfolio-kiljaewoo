7f37e86794c5760fc32441debeef7f6eb8040b13
wodn5515 <wodn5515@naver.com>
2020-12-10 23:06:19 +0900

    Add : Project serializer

--- portfolio-backend/data/views.py
+++ portfolio-backend/data/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
-from .models import Skill, SkillType
-from .serializers import SkillSerializer, SkillTypeSerializer
+from .models import Skill, SkillType, Project
+from .serializers import SkillSerializer, SkillTypeSerializer, ProjectSerializer
 
 # Create your views here.
 
@@ -14,3 +14,12 @@ class SkillList(viewsets.ModelViewSet):
 
     queryset = SkillType.objects.all()
     serializer_class = SkillTypeSerializer
+
+
+class ProjectList(viewsets.ModelViewSet):
+    """
+    프로젝트 조회
+    """
+
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
