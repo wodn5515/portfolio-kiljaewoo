ed579752d3eeba7cb3ad67a816c3c1e61d50df9d
wodn5515 <wodn5515@naver.com>
2021-01-09 17:37:01 +0900

    Add : thumbnail field to  project models

--- portfolio-backend/data/models.py
+++ portfolio-backend/data/models.py
@@ -35,6 +35,7 @@ class Skill(models.Model):
 class Project(models.Model):
     title = models.CharField(_("프로젝트명"), max_length=50, default="")
     desc = models.TextField(_("설명"), default="", blank=True)
+    thumbnail = models.ImageField(_("썸네일"), null=True)
     main_skill_used = models.ManyToManyField(Skill, verbose_name=_("주사용기술"), related_name="main_skill")
     sub_skill_used = models.ManyToManyField(Skill, verbose_name=_("보조사용기술"), related_name="sub_skill")
     github = models.CharField(_("깃헙주소"), max_length=255, default="")
